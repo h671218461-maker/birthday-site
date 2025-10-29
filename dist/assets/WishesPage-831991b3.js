ecf663aff91842c0512a98100e79f43324d05297
h671218461-maker <1695605702@qq.com>
2025-10-29 22:41:02 +0800

    deploy: update build files

--- dist/assets/WishesPage-831991b3.js
+++ dist/assets/WishesPage-831991b3.js
@@ -1,4 +1,4 @@
-import{_ as n,r as _,o as h,g as d,b as s,c as e,d as t,F as l,e as u,n as p,t as r}from"./index-72b15373.js";const g={class:"wishes-container"},x={class:"wishes-content"},y={class:"wishes-grid"},m={class:"wish-content"},v={class:"wish-text"},w={class:"wish-author"},f={__name:"WishesPage",setup(S){const c=_([{text:`"你就像大观园中最独特的那株绛珠仙草，为我的生命带来灵气与诗意。"
+import{_ as c,r as _,o as h,g as d,c as s,b as t,F as l,d as u,e,n as p,t as r}from"./main-83aea059.js";const g={class:"wishes-container"},x={class:"wishes-content"},y={class:"wishes-grid"},m={class:"wish-content"},v={class:"wish-text"},w={class:"wish-author"},f={__name:"WishesPage",setup(S){const n=_([{text:`"你就像大观园中最独特的那株绛珠仙草，为我的生命带来灵气与诗意。"
 
 在这个生日，愿我能如神瑛侍者般，用一生的呵护回报你的美好。
 祝我的"颦颦"生日快乐，永远不要"葬花"，永远笑靥如花。`,author:"《红楼梦》"},{text:`"月色溶溶夜，花阴寂寂春"
@@ -21,4 +21,4 @@ import{_ as n,r as _,o as h,g as d,b as s,c as e,d as t,F as l,e as u,n as p,t a
 `,author:"《牡丹亭》"},{text:`"金风玉露一相逢，便胜却人间无数"
 
 我们的相遇就是这般美好。生日快乐，愿我们不需要像牛郎织女那样等待七夕，
-而是"两情若是久长时，又岂在朝朝暮暮"的踏实相守。`,author:"《鹊桥仙》"}]);return h(()=>{d.fromTo(".wish-card",{y:24,opacity:0},{y:0,opacity:1,stagger:.12,duration:.6,ease:"power2.out"})}),(W,a)=>(s(),e("div",g,[t("div",x,[a[0]||(a[0]=t("h1",{class:"title"},"祝福墙",-1)),t("div",y,[(s(!0),e(l,null,u(c.value,(o,i)=>(s(),e("div",{key:i,class:"wish-card",style:p({animationDelay:`${i*.12}s`})},[t("div",m,[t("p",v,r(o.text),1),t("p",w,r(o.author),1)])],4))),128))])])]))}},k=n(f,[["__scopeId","data-v-b5c5d039"]]);export{k as default};
+而是"两情若是久长时，又岂在朝朝暮暮"的踏实相守。`,author:"《鹊桥仙》"}]);return h(()=>{d.fromTo(".wish-card",{y:24,opacity:0},{y:0,opacity:1,stagger:.12,duration:.6,ease:"power2.out"})}),(W,a)=>(e(),s("div",g,[t("div",x,[a[0]||(a[0]=t("h1",{class:"title"},"祝福墙",-1)),t("div",y,[(e(!0),s(l,null,u(n.value,(o,i)=>(e(),s("div",{key:i,class:"wish-card",style:p({animationDelay:`${i*.12}s`})},[t("div",m,[t("p",v,r(o.text),1),t("p",w,r(o.author),1)])],4))),128))])])]))}},k=c(f,[["__scopeId","data-v-9a20745b"]]);export{k as default};
